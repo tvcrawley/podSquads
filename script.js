97b5662cfcc4871481875b97e369f235f836c0f9
Toni Langley <chibia2@gmail.com>
2016-11-17 17:57:11 -0500

    can shuffle array

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+let wdi = ["alyassa", "ara", "anusone", "cliff", "dara", "david f", "david m", "julie", "jesse", "matt", "erik", "mason", "madlyn", "rafael", "patrick", "neil", "sandy"];
+
+function shuffle(a){
+  for(let i=a.length; i>0; i--){
+    let j = Math.floor(Math.random()*i);
+    [a[i - 1], a[j]] = [a[j], a[i - 1]];
+  }
+  console.log(a)
+}
+
+shuffle(wdi);
