1fdeb60940b3858f7b8881dc2c532b27585bab31
Toni Langley <chibia2@gmail.com>
2016-11-17 20:13:55 -0500

    data distributed into object arrays

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 
-let wdi = ["alyassa", "ara", "anusone", "cliff", "dara", "david f", "david m", "julie", "jesse", "matt", "erik", "mason", "madlyn", "rafael", "patrick", "neil", "sandy"];
+let wdi = ["alyassa", "anusone", "ara", "cliff", "dara", "david f", "david m",  "erik",  "jesse", "julie", "madlyn", "matt", "mason", "neil", "patrick", "rafael", "sandy"];
 
 function shuffle(a){
   for(let i=a.length; i>0; i--){
     let j = Math.floor(Math.random()*i);
     [a[i - 1], a[j]] = [a[j], a[i - 1]];
   }
-  console.log(a)
+  console.log(a);
+  return a;
 }
 
-shuffle(wdi);
+wdi = shuffle(wdi);
+
+let pod1 =[];
+let pod2 = [];
+let pod3 = [];
+let pod4 = [];
+
+let pods = {
+  one: [],
+  two: [],
+  three: [],
+  four: []
+};
+
+
+  // console.log(student);
+  while(wdi.length>0){
+  for(let pod in pods){
+    if(pods[pod].length <5){
+    pods[pod].push(wdi.shift());
+    // console.log(pod + " " + pods[pod]);
+    // console.log(pods[pod].findIndex(4))
+    // if(pods[pod][4] === "undefined"){
+    //   pods[pod].pop();
+    // }
+  }
+  }
+  }
