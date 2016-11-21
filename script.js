2a8710239af5a11b6c7569fcfb4dda6843d29db6
Toni Langley <chibia2@gmail.com>
2016-11-21 09:39:25 -0500

    updated wdi array

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function shuffle(a){
-  for(let i=a.length; i>0; i--){
-    let j = Math.floor(Math.random()*i);
-    [a[i - 1], a[j]] = [a[j], a[i - 1]];
-  }
-  console.log(a);
-  return a;
+function shuffle(a) {
+    for (let i = a.length; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+    console.log(a);
+    return a;
 }
 
 let node = document.createElement("h4");
@@ -14,33 +14,33 @@ let element = document.getElementsByTagName("header")[0];
 element.appendChild(node);
 // element.insertBefore(node, element.childNodes[2])
 
-document.getElementsByTagName("h1")[0].addEventListener("click", function(){
+document.getElementsByTagName("h1")[0].addEventListener("click", function() {
 
-  let wdi = ["alyssa", "anusone", "ara", "clifton", "dara", "david f", "david m",  "erik",  "jesse", "julie", "madlyn", "matthew", "mason", "neil", "patrick", "rafael", "sandy"];
+    let wdi = ["alyssa", "anusone", "ara", "clifton", "dara", "david f", "david m", "erik", "jesse", "julie", "matthew", "mason", "neil", "patrick", "rafael", "sandy"];
 
-  wdi = shuffle(wdi);
+    wdi = shuffle(wdi);
 
-  let pods = {
-    one: [],
-    two: [],
-    three: [],
-    four: []
-  };
+    let pods = {
+        one: [],
+        two: [],
+        three: [],
+        four: []
+    };
 
-while(wdi.length>0){
-  for(let pod in pods){
+    while (wdi.length > 0) {
+        for (let pod in pods) {
 
-    if(pods[pod].length <5){
-      pods[pod].push(wdi.shift());
+            if (pods[pod].length < 5) {
+                pods[pod].push(wdi.shift());
 
-      if(pods[pod].lastIndexOf()> 0){
-        pods[pod].pop();
-      }
+                if (pods[pod].lastIndexOf() > 0) {
+                    pods[pod].pop();
+                }
+            }
+            document.getElementById(pod).innerHTML = pods[pod].join(", ");
+
+        }
     }
-     document.getElementById(pod).innerHTML = pods[pod].join(", ");
-
-}
-}
 
 });
 
